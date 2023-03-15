be08f2414173bc7cd2606cb98c156f416b777366
Nitesh Balusu <niteshbalusu@icloud.com>
2023-03-15 16:06:31 -0400

    add nip05 verification
    Signed-off-by: Nitesh Balusu <niteshbalusu@icloud.com>

--- src/main.rs
+++ src/main.rs
@@ -30,65 +30,65 @@ async fn main() {
     let lnd = get_lnd().await;
     test_invoice(lnd).await;
 
-    match get_nostr_keys() {
-        Ok((privkey, pubkey)) => {
-            let relays = vec![
-                "wss://nostr.foundrydigital.com",
-                "wss://eden.nostr.land",
-                "wss://relay.damus.io",
-                "wss://relay.snort.social",
-                "wss://nos.lol",
-                "wss://relay.current.fyi",
-                "wss://relay.nostr.info",
-                "wss://nostr.zebedee.cloud",
-                "wss://nostr.fmt.wiz.biz",
-                "wss://relay.nostr.bg",
-                "wss://nostr.mom",
-                "wss://nostr.bitcoiner.social",
-                "wss://nostr.oxtr.dev",
-                "wss://no.str.cr",
-            ];
+    // match get_nostr_keys() {
+    //     Ok((privkey, pubkey)) => {
+    //         let relays = vec![
+    //             "wss://nostr.foundrydigital.com",
+    //             "wss://eden.nostr.land",
+    //             "wss://relay.damus.io",
+    //             "wss://relay.snort.social",
+    //             "wss://nos.lol",
+    //             "wss://relay.current.fyi",
+    //             "wss://relay.nostr.info",
+    //             "wss://nostr.zebedee.cloud",
+    //             "wss://nostr.fmt.wiz.biz",
+    //             "wss://relay.nostr.bg",
+    //             "wss://nostr.mom",
+    //             "wss://nostr.bitcoiner.social",
+    //             "wss://nostr.oxtr.dev",
+    //             "wss://no.str.cr",
+    //         ];
 
-            let content = format!(
-                "{{\"name\": \"{}\", \"nip05\": \"{}@{}\"}}",
-                username.as_ref().unwrap(),
-                username.as_ref().unwrap(),
-                domain.as_ref().unwrap()
-            );
+    //         let content = format!(
+    //             "{{\"name\": \"{}\", \"nip05\": \"{}@{}\"}}",
+    //             username.as_ref().unwrap(),
+    //             username.as_ref().unwrap(),
+    //             domain.as_ref().unwrap()
+    //         );
 
-            println!("{}", content);
+    //         println!("{}", content);
 
-            let current_time = SystemTime::now().duration_since(UNIX_EPOCH).unwrap();
-            let timestamp = current_time.as_secs();
+    //         let current_time = SystemTime::now().duration_since(UNIX_EPOCH).unwrap();
+    //         let timestamp = current_time.as_secs();
 
-            let id = calculate_id(json!([0, pubkey, timestamp, 0, [], content]));
+    //         let id = calculate_id(json!([0, pubkey, timestamp, 0, [], content]));
 
-            let nip05_json = json!([
-                "EVENT",
-                {
-                    "content": content,
-                    "created_at": timestamp,
-                    "id": id,
-                    "kind": 0,
-                    "pubkey": pubkey,
-                    "tags": [],
-                    "sig": sign_message(privkey, &id)
-                },
-            ]);
+    //         let nip05_json = json!([
+    //             "EVENT",
+    //             {
+    //                 "content": content,
+    //                 "created_at": timestamp,
+    //                 "id": id,
+    //                 "kind": 0,
+    //                 "pubkey": pubkey,
+    //                 "tags": [],
+    //                 "sig": sign_message(privkey, &id)
+    //             },
+    //         ]);
 
-            let relay_string: Vec<String> = relays.iter().map(|s| s.to_string()).collect();
+    //         let relay_string: Vec<String> = relays.iter().map(|s| s.to_string()).collect();
 
-            let publish_message = serde_json::to_string(&nip05_json)
-                .expect("Failed to serialize response body to JSON");
+    //         let publish_message = serde_json::to_string(&nip05_json)
+    //             .expect("Failed to serialize response body to JSON");
 
-            tokio::spawn(async move {
-                publish(relay_string, publish_message).await;
-            });
+    //         tokio::spawn(async move {
+    //             publish(relay_string, publish_message).await;
+    //         });
 
-            pubkey
-        }
-        Err(_) => "".to_string(),
-    };
+    //         pubkey
+    //     }
+    //     Err(_) => "".to_string(),
+    // };
 
     start_server().await;
 
