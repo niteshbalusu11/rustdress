c062d35defec879c1d12967552f7d0be297360e1
Jordan Bravo <jordan@bravo.cc>
2025-11-01 08:10:32 -0400

    update workflow

--- flake.nix
+++ flake.nix
@@ -6,11 +6,10 @@
   inputs.nixpkgs-cmake-3-29.url = "github:nixos/nixpkgs/d4f247e89f6e10120f911e2e2d2254a050d0f732";
 
   outputs =
-    {
-      flake-utils,
-      nixpkgs,
-      nixpkgs-cmake-3-29,
-      ...
+    { flake-utils
+    , nixpkgs
+    , nixpkgs-cmake-3-29
+    , ...
     }:
     flake-utils.lib.eachDefaultSystem (
       system:
@@ -32,18 +31,6 @@
             rustc
             rustfmt
           ];
-          # nativeBuildInputs = with pkgs; [
-          #   bacon
-          #   cargo
-          #   clippy
-          #   pkgs-cmake-pkgs-cmake-3-29.cmake
-          #   git
-          #   openssl
-          #   pkg-config
-          #   rust-analyzer
-          #   rustc
-          #   rustfmt
-          # ];
         };
       }
     );
